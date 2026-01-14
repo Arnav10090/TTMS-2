51de23fbf2c33a97727e49a58ec1f1e02e5db846
Arnav Tiwari <arnavt292@gmail.com>
2026-01-14 10:31:49 +0530

    Updated UI

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ const App = () => {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+      <ThemeProvider attribute="class" defaultTheme="light" forcedTheme="light" enableSystem={false}>
         <TooltipProvider>
           <Toaster />
           <Sonner />
